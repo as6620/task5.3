59f3ce3e3763771a62233890820b1dd83e376387
as6620 <as6620@bs.amalnet.k12.il>
2023-12-21 15:47:06 +0200

    final

--- app/src/main/java/com/example/task53/MainActivity.java
+++ app/src/main/java/com/example/task53/MainActivity.java
@@ -10,13 +10,22 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     Button btn;
     TextView tv;
+    int counterClick = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+        tv = findViewById(R.id.tv);
     }
 
     public void clicked(View view) {
-
+        counterClick +=1;
+        if (counterClick != 6)
+            tv.setText("This is a click number:" + counterClick);
+        else {
+            tv.setText("Enough to click. Go to new start!");
+            counterClick = 0;
+        }
     }
 }
